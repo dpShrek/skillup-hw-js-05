253db3f8cae253155676a73b03c4bbbfbf6e5f0e
Paniuta.p <paniuta.p@gmail.com>
2020-05-06 17:28:26 +0300

    10-task

--- task-10.js
+++ task-10.js
@@ -1,9 +1,40 @@
 /*
 
+Напиши функцию countProps(obj), считающую кол-во свойств в объекте. Функция возвращает число - количество свойств.
+
+const countProps = function(obj) {
+  // твой код
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+
+ */
 
 
-*/
+
+
+let gggg = { mail: 'poly@mail.com', isOnline: true, score: 500 };
 
 
 console.log ('');
 console.log ('10 задание');
+
+function countProps (argyment) {
+    
+    let counter = 0;
+        for (let i in argyment) {
+            counter++;
+}
+
+console.log( "Всего свойств: " + counter );
+        
+}
+
+
+countProps({}); // 0
+
+countProps({ name: 'Mango', age: 2 }); // 2
+
+countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }); // 3
+
