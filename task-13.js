b93cf877b99509f310ce89e90d0072f78ab42f67
Paniuta.p <paniuta.p@gmail.com>
2020-05-18 13:34:36 +0300

    all-task

--- task-13.js
+++ task-13.js
@@ -1,9 +1,54 @@
 /*
 
+Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов и имя свойства. Возвращает массив значений определенного свойства prop из каждого объекта в массиве.
 
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
 
-*/
+const getAllPropValues = function(arr, prop) {
+  // твой код
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
 
 
 console.log ('');
 console.log ('13 задание');
+
+const products123 = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Сканер', price: 2700, quantity: 3 },
+    { name: 'Дроид', price: 400, quantity: 7 },
+    { name: 'Захват', price: 1200, quantity: 2 },
+  ];
+
+const getAllPropValues = function(arr, prop) {
+    let gg= [];
+
+      for (let i=0; i<arr.length; i++){
+
+        if (arr == undefined || arr[i][prop] == undefined){
+        break;
+          
+        }
+
+        else 
+        gg.push(arr[i][prop]);
+        
+      }
+
+    return gg;
+};
+
+
+console.log(getAllPropValues(products123, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products123, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products123, 'category')); // []
--- task-14.js
+++ task-14.js
@@ -1,9 +1,47 @@
-/*
 
+/*Напиши функцию calculateTotalPrice(allProdcuts, productName), которая получает массив объектов и имя продукта (значение свойства name). Возвращает общую стоимость продукта (цена * количество).
 
+Вызовы функции для проверки работоспособности твоей реализации.
 
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const calculateTotalPrice = function(allProdcuts, productName) {
+  // твой код
+};
+
+Вызовы функции для проверки работоспособности твоей реализации.
+ 
 */
+
+
 
 
 console.log ('');
 console.log ('14 задание');
+
+
+const products11 = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Сканер', price: 2700, quantity: 3 },
+    { name: 'Дроид', price: 400, quantity: 7 },
+    { name: 'Захват', price: 1200, quantity: 2 },
+  ];
+  
+  const calculateTotalPrice = function(allProdcuts, productName) {
+      for (let i=0; i<allProdcuts.length; i++){   
+        if (allProdcuts && allProdcuts[i].name == productName) {
+          return ((allProdcuts[i].price) * (allProdcuts[i].quantity));
+        }
+       
+      }
+    
+  };
+
+console.log(calculateTotalPrice(products11, 'Радар')); // 5200
+
+console.log(calculateTotalPrice(products11, 'Захват')); // 2800
